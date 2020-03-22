3f263e7792659a8a52f4598ad13368684cc3cfd4
Ben Anderson <andersjbe@gmail.com>
2020-03-22 23:44:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-iterativeLog(array) {
+function iterativeLog(array) {
   array.forEach((item, index) => {
     console.log(`${index}: ${item}`);
   });
+}
+
+function iterate(callback) {
+  array = [527, 9762, 290, 1646, 9734];
+  array.forEach(item => {
+    callback(item);
+  });
+  return array;
 }
